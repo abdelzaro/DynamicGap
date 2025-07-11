ac8433813e34d0c3e55184c6daa821e180e54216
abdelzaro <abdelzaro8@gmail.com>
2025-07-11 09:50:38 -0400

    added more and launch for testing

---
+++ build/catkin_tools_prebuild/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/power20/dynamic_gap/build/catkin_tools_prebuild/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
